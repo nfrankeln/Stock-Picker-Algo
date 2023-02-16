fadd940493be8ccea876268faa06a982e22c7a22
nfrankeln <ntnyfranky@gmail.com>
2023-02-16 14:19:18 -0600

    added python loop solution O(n)^2

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,15 @@
+# for loop method
 def picker(prices):
-    pass
+    max_profits = 0
+    buy_sell_days = []
+
+    for first_day , first_price in enumerate(prices):
+        for second_day , second_price  in enumerate (prices):
+            if second_day < first_day:
+                continue
+            if second_price - first_price > max_profits:
+                max_profits = second_price - first_price
+                buy_sell_days = [first_day,second_day]
+    return buy_sell_days
+
+print(picker([3,2,3,4,1,8]))
