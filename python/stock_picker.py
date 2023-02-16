ecaaf2468d8ae2e18c792655b72681cc8297b695
nfrankeln <ntnyfranky@gmail.com>
2023-02-16 14:37:44 -0600

    added single loop solution to python

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,5 +1,5 @@
 # for loop method
-def picker(prices):
+def picker_double_loop(prices):
     max_profits = 0
     buy_sell_days = []
 
@@ -11,5 +11,20 @@ def picker(prices):
                 max_profits = second_price - first_price
                 buy_sell_days = [first_day,second_day]
     return buy_sell_days
+
+# single loop method
+def picker(prices):
+    max_profits = 0
+    buy_sell_days = []
+    lowest_day = 0
+    
+    for current_day , current_price in enumerate(prices):
+        if current_price < prices [lowest_day]:
+            lowest_day = current_day
+        if current_price - prices [lowest_day] > max_profits:
+            max_profits = current_price - prices [lowest_day]
+            buy_sell_days = [lowest_day,current_day]
+    return buy_sell_days
+
 
 print(picker([3,2,3,4,1,8]))
